5261acd08d8cabe3556509e94c126c494a46f885
Duzy-Dym <aleksanderowsinski23@gmail.com>
2024-09-10 16:13:26 +0200

    Cwiczenie 3 properties

---
+++ cw3/Person.cs
@@ -0,0 +1,24 @@
+﻿public class Person {
+    private string firstName { get; set; }
+    private string lastName { get; set; }
+
+    public Person(string firstName, string lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person(){
+        this.firstName = "";
+        this.lastName = "";
+    }
+
+    public override string ToString(){
+        return firstName + " " + lastName;
+    }
+
+    public string FirstName {
+        get { return firstName; }
+        set { firstName = value; }
+    }
+    
+}
---
+++ cw3/Program.cs
@@ -0,0 +1,5 @@
+﻿Person p1 = new Person();
+Person p2 = new Person("Jan","Kowalski");
+Console.WriteLine(p2);
+p2.FirstName = "Kacper";
+Console.WriteLine(p2);
