6a959fa00932848690b84906d21204858dd79fe5
Duzy-Dym <aleksanderowsinski23@gmail.com>
2024-09-10 15:11:19 +0200

    cw2

---
+++ cw2/Program.cs
@@ -0,0 +1,20 @@
+﻿void cw1(){
+    Console.Write("Podaj a: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Liczba {a} do potęgi wynosi {a*a}");
+}
+
+void cw2()
+{
+    int a, b;
+    Console.WriteLine("Podaj a: ");
+    a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Podaj b: ");
+    b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"{a} + {b} = {a + b}");
+    Console.WriteLine($"{a} - {b} = {a - b}");
+    Console.WriteLine($"{a} * {b} = {a * b}");
+    Console.WriteLine($"{a} / {b} = {(float)a / b}");
+}
+// cw1();
+cw2();
