6c8fa209dea38845b75dd54779d92cd233553821
Duzy-Dym <aleksanderowsinski23@gmail.com>
2024-09-17 16:14:01 +0200

    nwm czy to dziala

---
+++ cw4/Person.cs
@@ -0,0 +1,36 @@
+﻿public class Person {
+    private string firstName { get; set; }
+    private string lastName { get; set; }
+    private int age;
+
+    public Person(string firstName, string lastName, int age = 18){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Person(){
+        this.firstName = "";
+        this.lastName = "";
+        this.age = 18;
+    }
+
+    public override string ToString(){
+        return $"{FirstName} {LastName} wiek: {Age}";
+    }
+
+    public string FirstName {
+        get { return firstName.ToUpper(); }
+        set { firstName = value; }
+    }
+
+    public string LastName {
+        get { return lastName; }
+        set { lastName = value; }
+    }
+
+    public int Age {
+        get { return age; }
+        set { age = value < 0 ? -value : value; }
+    }
+}
--- cw4/Program.cs
+++ cw4/Program.cs
@@ -3,11 +3,18 @@ for (int i = 0; i < tab.Length; i++) {
     tab[i] = i * i;
 }
 string[] tab2 = new string[] {"ala","ma","Kota","i","plot","ale","jaja","rety","Kotlety"};
-int[] tab3 = generTab(10);
-
+int[] tab3 = GenerTab(10);
+Person[] tab4 = new Person[] { new Person("Jan", "Kowalski", 20), new Person(), new Person("Kacper", "Nowak") };
 showTable(tab);
 showTable(tab2);
 showTable(tab3);
+showTable(tab4);
+Console.WriteLine("Max = " + GetMax(tab3));
+Console.WriteLine("Min = " + GetMin(tab3));
+Console.WriteLine("Amplitude = " + (GetMax(tab3) + GetMin(tab3)));
+Console.WriteLine("Avg = " + GetAvg(tab3));
+showTable(GenerDivBy(10,5));
+
 void showTable<T>(T[] tab){
     foreach (T element in tab) {
         Console.Write(element+" ");
@@ -15,12 +22,48 @@ void showTable<T>(T[] tab){
     Console.WriteLine();
 
 }
-int[] generTab(int size){
+int[] GenerTab(int size){
     Random rnd = new Random();
     int[] tab = new int[size];
     for (int i = 0; i < tab.Length; i++) {
         tab[i] = rnd.Next(100);
     }
+    return tab;
+}
+int GetMax(int[] arr){
+    int max = arr[0];
+    for (int i = 1; i < arr.Length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+int GetMin(int[] arr){
+    int min = arr[0];
+    for (int i = 1; i < arr.Length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
 
+    return min;
+}
+double GetAvg(int[] arr){
+    return (double)GetSum(arr) / arr.Length;
+}
+int GetSum(int[] arr){
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+int[] GenerDivBy(int size, int div){
+    Random rnd = new Random();
+    int[] tab = new int[size];
+    for (int i = 0; i < tab.Length; i++) {
+        tab[i] = rnd.Next(100/div)*div;
+    }
     return tab;
 }
