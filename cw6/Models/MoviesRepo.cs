cafadc5ad3af2bd45270d6ba47fba155507ff33c
MiszukUwU <aleksanderowsinski23@gmail.com>
2024-10-22 16:15:19 +0200

    Dodawanie filmow

--- cw6/Models/MoviesRepo.cs
+++ cw6/Models/MoviesRepo.cs
@@ -17,4 +17,25 @@ public class MoviesRepo {
             return _movies;
         }
     }
+    
+    private void SaveChanges(){
+         var options = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+        string content = JsonSerializer.Serialize(_movies, options);
+        File.WriteAllText(_filePath, content);
+    }
+    
+    private int GetNextId(){
+        return _movies != null ?_movies.Max(m => m.Id) +1 : 1;
+    }
+    public void AddMovie(Movie movie){
+        if (_movies == null){
+            _movies = new List<Movie>();
+        }
+        movie.Id = GetNextId();
+        _movies.Add(movie);
+        SaveChanges();
+    }
 }
--- cw6/Pages/AddMovie.cshtml.cs
+++ cw6/Pages/AddMovie.cshtml.cs
@@ -4,10 +4,31 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace cw6.Pages;
 
-public class AddMovie : PageModel {
+public class AddMovieModel : PageModel {
     [BindProperty]
     public Movie? MyMovie { get; set; }
+    public List<string>? Genres {get; set;}
     public void OnGet(){
-        
+        ViewData["Genres"] = Genres;
+        ViewData["Message"] = "Dopiero wywitlamy formularz";
+    }
+    public IActionResult onPost(){
+        ViewData["Genres"] = Genres;
+        if (MyMovie == null){
+            ViewData["Message"] = "Brak danych";
+            return Page();
+        }
+        if(ModelState.IsValid){
+            MoviesRepo repo = new MoviesRepo("movies.json");
+            repo.AddMovie(MyMovie);
+            ViewData["Message"] = "Movie added";
+            return RedirectToPage("Movies");
+        } else {
+            ViewData["Message"] = "Invalid movie";
+        }
+        return Page();      
+    }
+    public AddMovieModel(){
+        Genres = new List<string>{"Acton", "Crime", "Sci-Fi", "Drama", "Action", "Fantasy"};
     }
 }
