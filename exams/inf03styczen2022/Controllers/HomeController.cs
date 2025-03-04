db4da3a0406f53cfa2ee9b5915f8ee96aaf43444
Miszukuwu <aleksanderowsinski23@gmail.com>
2025-03-04 16:12:00 +0100

    start of 2022 styczen zad 1

---
+++ exams/inf03styczen2022/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace inf03styczen2022Controllers;
+
+using inf03styczen2022.Models;
+using MySql.Data.MySqlClient;
+
+public class HomeController : Controller {
+    private readonly string _connectionString;
+    public HomeController(IConfiguration configuration) {
+        _connectionString = configuration.GetConnectionString("mysql");
+    }
+    // GET
+    public IActionResult Index() {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Rezerwacja(Reservation reservation) {
+        MySqlConnection connection = new MySqlConnection();
+        return View();
+    }
+}
---
+++ exams/inf03styczen2022/Models/Reservation.cs
@@ -0,0 +1,9 @@
+﻿namespace inf03styczen2022.Models;
+
+public class Reservation {
+    public int Id { get; set; }
+    public int TableNumber { get; set; }
+    public string? ReservationDate { get; set; }
+    public int AmountOfPeople { get; set; }
+    public string? PhoneNumber { get; set; }
+}
