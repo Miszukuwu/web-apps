c639a1d7aa032fcb061a9708340ffe5ae1824fce
Miszukuwu <aleksanderowsinski23@gmail.com>
2025-03-25 16:15:05 +0100

    start of exam

---
+++ exams/inf03/01styczen2021/Controllers/FootballController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace inf03styczen202101.Controllers;
+
+using Models;
+
+public class FootballController : Controller {
+    private FootballRepo _footballRepo;
+    public FootballController(IConfiguration configuration) {
+        _footballRepo = new FootballRepo(configuration.GetConnectionString("FootballDB"));
+    }
+
+    [HttpGet]
+    public IActionResult Index() {
+        List<Match> matches = _footballRepo.GetMatches();
+        ViewBag.Matches = matches;
+        ViewBag.Players = new List<Player>();
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Index(int id) {
+        List<Match> matches = _footballRepo.GetMatches();
+        List<Player> players = _footballRepo.GetPositionPlayers(id);
+        ViewBag.Matches = matches;
+        ViewBag.Players = players;
+        return View();
+    }
+}
